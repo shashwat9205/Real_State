cce9215d2c57e4da560579251b3a6ccfbcaa2b15
Prajapati-Dharmendra97095 <dharmendrakumarmp_cse21@its.edu.in>
2026-02-12 15:53:18 +0530

    dynamic property update

--- update.php
+++ update.php
@@ -14,27 +14,63 @@
     <?php
     include 'config.php';
     $id = $_GET['id'];
-    $Record = mysqli_query($conn, "SELECT * FROM `logoimage` WHERE id = $id");
+    $Record = mysqli_query($conn, "SELECT * FROM `property` WHERE id = $id");
     $data = mysqli_fetch_array($Record);
-
     ?>
 
     <section>
 
         <div class="admin-main">
-            <h1>Room ADD</h1>
+            <h1>Property Update</h1>
             <form action="update1.php" method="post" enctype="multipart/form-data">
-            
-                <label for="para">Content</label>
-                <input type="text" name="para" value="<?php echo $data['para']; ?>" placeholder="Enter Content for Footer"> <br><br>
+                <div class="All-box" style="display: flex;">
+                    <div class="All-1">
+                        <label for="">Features1</label>
+                        <input type="text" name="featured1" value="<?php echo $data['featured1']; ?>" placeholder="Enter Featured 1"> <br><br>
 
-                <label for="image">Image :</label>
-                <input type="file" name="image" value="<?php echo $data['image'] ?>" required>
-                <img src="<?php echo $data['image'] ?>" alt="" height="200" width="250"><br><br>
+                        <label for="">Features2</label>
+                        <input type="text" name="featured2" value="<?php echo $data['featured2']; ?>" placeholder="Enter Featured 2"> <br><br>
 
-                    <input type="hidden" name="id" value="<?php echo $data['id'] ?>">
+                        <label for="">Title</label>
+                        <input type="text" name="title" value="<?php echo $data['title']; ?>" placeholder="Enter Heading"> <br><br>
 
-                    <button type="submit" name="update" class="btn btn-danger">Update</button>
+                        <label for="">Para</label>
+                        <input type="text" name="para" value="<?php echo $data['para']; ?>" placeholder="Enter Para"> <br><br>
+
+                        <label for="">Description</label>
+                        <input type="text" name="description" value="<?php echo $data['description']; ?>" placeholder="Enter description"> <br><br>
+
+                        <label for="">SQ-Ft</label>
+                        <input type="text" name="sq_fit" value="<?php echo $data['sq_fit']; ?>" placeholder="Enter SQ-Ft"> <br><br>
+
+                    </div>
+                    <div class="All-2">
+                        <label for="">Total Rooms</label>
+                        <input type="text" name="rooms" value="<?php echo $data['rooms']; ?>" placeholder="Enter Total Rooms"> <br><br>
+
+                        <label for="">Established Year</label>
+                        <input type="text" name="year" value="<?php echo $data['year']; ?>" placeholder="Enter Established Year"> <br><br>
+
+                        <label for="">Bathroom Year</label>
+                        <input type="text" name="bathroom" value="<?php echo $data['bathroom']; ?>" placeholder="Enter SQ-Ft"> <br><br>
+
+                        <label for="">Garage</label>
+                        <input type="text" name="garage" value="<?php echo $data['garage']; ?>" placeholder="Enter Garage"> <br><br>
+
+                        <label for="">Slug</label>
+                        <input type="text" name="slug" value="<?php echo $data['slug']; ?>" placeholder="Enter Slug"> <br><br>
+
+                        <label for="image">Image :</label>
+                        <input type="file" name="image" value="<?php echo $data['image'] ?>" required>
+                        <img src="<?php echo $data['image'] ?>" alt="" height="200" width="250"><br><br>
+
+                        <input type="hidden" name="id" value="<?php echo $data['id'] ?>">
+
+                        <button type="submit" name="update_property" class="btn btn-danger">Update</button>
+                    </div>
+                </div>
+
+
             </form>
         </div>
     </section>
--- update1.php
+++ update1.php
@@ -1,121 +1,20 @@
 <?php
 include 'config.php';
 
-if (isset($_POST['update'])) {
+if (isset($_POST['update_property'])) {
 
     $id    = $_POST['id'];
-    $para  = $_POST['para'];
-
-    // Image upload
-    $img_loc  = $_FILES['image']['tmp_name'];
-    $img_name = $_FILES['image']['name'];
-
-    if (!empty($img_name)) {
-        $img_des = "uploadimage/" . $img_name;
-        move_uploaded_file($img_loc, $img_des);
-    } else {
-        $img_des = $_POST['old_image'];
-    }
-
-    $update_query = "
-        UPDATE `logoimage`
-        SET 
-            `para` = '$para',
-            `image` = '$img_des'
-        WHERE `id` = $id
-    ";
-
-    mysqli_query($conn, $update_query);
-
-    header("Location: admin_page.php");
-}
-
-
-// Home Services Update 
-if (isset($_POST['update_home_services'])) {
-
-    $id    = $_POST['id'];
+    $FEATURED1  = $_POST['featured1'];
+    $FEATURED2  = $_POST['featured2'];
     $TITLE  = $_POST['title'];
     $PARA  = $_POST['para'];
-    $LINK  = $_POST['link'];
-
-    // Image upload
-    $img_loc  = $_FILES['image']['tmp_name'];
-    $img_name = $_FILES['image']['name'];
-
-    if (!empty($img_name)) {
-        $img_des = "uploadimage/" . $img_name;
-        move_uploaded_file($img_loc, $img_des);
-    } else {
-        $img_des = $_POST['old_image'];
-    }
-
-    $update_query = "
-        UPDATE `home_services`
-        SET 
-            `title` = '$TITLE',
-            `para` = '$PARA',
-            `link` = '$LINK',
-            `image` = '$img_des'
-        WHERE `id` = $id
-    ";
-
-    mysqli_query($conn, $update_query);
-
-    header("Location: dashboard.php");
-}
-
-
-// SIngle Blog Update 
-if (isset($_POST['update_main_blog'])) {
-
-    $id    = $_POST['id'];
-    $TITLE  = $_POST['title'];
-    $PARA1  = $_POST['para1'];
-    $PARA2  = $_POST['para2'];
-    $PARA3  = $_POST['para3'];
-    $BLOG_DATE = $_POST['blog_date'];
-    $LINK  = $_POST['link'];
-
-    // Image upload
-    $img_loc  = $_FILES['image']['tmp_name'];
-    $img_name = $_FILES['image']['name'];
-
-    if (!empty($img_name)) {
-        $img_des = "uploadimage/" . $img_name;
-        move_uploaded_file($img_loc, $img_des);
-    } else {
-        $img_des = $_POST['old_image'];
-    }
-
-    $update_query = "
-        UPDATE `main_blogs`
-        SET 
-            `title` = '$TITLE',
-            `para1` = '$PARA1',
-            `para2` = '$PARA2',
-            `para3` = '$PARA3',
-            `blog_date` = '$BLOG_DATE',
-            `link` = '$LINK',
-            `image` = '$img_des'
-        WHERE `id` = $id
-    ";
-
-    mysqli_query($conn, $update_query);
-
-    header("Location: dashboard.php");
-}
-
-
-// Team Member Update 
-if (isset($_POST['update_team'])) {
-
-    $id    = $_POST['id'];
-    $NAME  = $_POST['name'];
-    $POSITION  = $_POST['position'];
     $DESCRIPTION  = $_POST['description'];
-    $MOBILE  = $_POST['mobile'];
-    $EMAIL  = $_POST['email'];
+    $SQ_FIT  = $_POST['sq_fit'];
+    $ROOMS  = $_POST['rooms'];
+    $YEAR  = $_POST['year'];
+    $BATHROOM  = $_POST['bathroom'];
+    $GARAGE  = $_POST['garage'];
+    $SLUG  = $_POST['slug'];
 
     // Image upload
     $img_loc  = $_FILES['image']['tmp_name'];
@@ -129,18 +28,24 @@ if (isset($_POST['update_team'])) {
     }
 
     $update_query = "
-        UPDATE `team`
+        UPDATE `property`
         SET 
-            `name` = '$NAME',
-            `position` = '$POSITION',
+            `featured1` = '$FEATURED1',
+            `featured2` = '$FEATURED2',
+            `title` = '$TITLE',
+            `para` = '$para',
             `description` = '$DESCRIPTION',
-            `email` = '$EMAIL',
-            `mobile` = '$MOBILE',
+            `sq_fit` = '$SQ_FIT',
+            `rooms` = '$ROOMS',
+            `year` = '$YEAR',
+            `bathroom` = '$BATHROOM',
+            `garage` = '$GARAGE',
+            `slug` = '$SLUG',
             `image` = '$img_des'
         WHERE `id` = $id
     ";
 
     mysqli_query($conn, $update_query);
 
-    header("Location: dashboard.php");
+    header("Location: Dashboard.php");
 }
